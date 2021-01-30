39ec9c9008053f117400f95fb3084ab2acd19665
Johannes Nilsson <johannes@nilsson.im>
2021-01-30 22:42:08 +0100

    Add AddNew, Created and SaveData

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -25,7 +25,7 @@ namespace Flixer.Controllers
             return View(movies);
         }
 
-        [Route("Movie/{id}")]
+        [Route("Movie/Details/{id}")]
         public IActionResult SingleMovie(int id)
         {
             // Get data from session storage and deserialize it
@@ -37,11 +37,80 @@ namespace Flixer.Controllers
             return View(m);
         }
 
+        [HttpGet]
+        public IActionResult AddNew()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNew([FromForm]Movie m)
+        {
+            try
+            {
+                /* TODO:
+                 * Move most of this code over to the SaveData method
+                 */
+
+                // Get data from session storage and deserialize it
+                string data = HttpContext.Session.GetString("movies");
+                var movies = JsonConvert.DeserializeObject<List<Movie>>(data);
+
+                // Add the new movie to the list
+                movies.Add(m);
+
+                // Serialize the updated data
+                var updatedMovies = JsonConvert.SerializeObject(movies);
+
+                // Overwrite the session storage
+                HttpContext.Session.SetString("movies", updatedMovies);
+
+                // Save data to file
+                SaveData(updatedMovies);
+
+                // Save the new movie in session storage
+                var newMovie = JsonConvert.SerializeObject(m);
+                HttpContext.Session.SetString("newMovie", newMovie);
+
+                // Redirect to Created
+                return RedirectToAction("Created");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult Created()
+        {
+            /* TODO: Is it possible to pass the data between controllers
+             * in a simpler way? It feels so ugly to serialize and then
+             * deserialize constantly....
+             */
+
+            // Get the newly added movie from session storage
+            string newMovie = HttpContext.Session.GetString("newMovie");
+
+            // Deserialize the JSON and pass the movie object to view via ViewBag
+            Movie latestMovie = JsonConvert.DeserializeObject<Movie>(newMovie);
+            ViewBag.LatestMovie = latestMovie;
+
+            // Return view
+            return View();
+        }
+
         public string GetData()
         {
             // Open the file and return all of its contents
-            string json = System.IO.File.ReadAllText("movies.json");
-            return json;
+            string data = System.IO.File.ReadAllText("movies.json");
+            return data;
+        }
+
+        public void SaveData(string data)
+        {
+            System.IO.File.WriteAllText("movies.json", data);
+            return;
         }
     }
 }
