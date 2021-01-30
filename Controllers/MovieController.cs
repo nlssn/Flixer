8b7c5c9c2a5aba58032b910b06c37ebbb34fc36f
Johannes Nilsson <johannes@nilsson.im>
2021-01-30 15:03:34 +0100

    Add SingleMovie

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Flixer.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -13,13 +14,29 @@ namespace Flixer.Controllers
         [Route("Movies")]
         public IActionResult Index()
         {
-            string moviesJson = GetData();
-            ViewData["json"] = moviesJson;
-            return View();
+            // Retrieve the data from the JSON-file
+            string data = GetData();
+
+            // Store the data in session storage for later use
+            HttpContext.Session.SetString("movies", data);
+
+            // Deserialize the data and return it with the view
+            var movies = JsonConvert.DeserializeObject<List<Movie>>(data);
+            return View(movies);
+        }
+
+        [Route("Movie/{id}")]
+        public IActionResult SingleMovie(int id)
+        {
+            string data = HttpContext.Session.GetString("movies");
+            var movies = JsonConvert.DeserializeObject<List<Movie>>(data);
+            Movie m = movies.Find(x => x.Id == id);
+            return View(m);
         }
 
         public string GetData()
         {
+            // Open the file and return all of its contents
             string json = System.IO.File.ReadAllText("movies.json");
             return json;
         }
