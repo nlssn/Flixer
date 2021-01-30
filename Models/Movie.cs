c1473b01e7f5189db1e487e9fdbacdda74027669
Johannes Nilsson <johannes@nilsson.im>
2021-01-30 12:54:22 +0100

    Add Movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Flixer.Models
+{
+    public class Movie
+    {
+        // Properties
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+        public bool Watched { get; set; }
+        public int Score { get; set; }
+
+        // Constructor
+        public Movie(int id, string title, string genre, string year, bool watched, int score)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Genre = genre;
+            this.Year = year;
+            this.Watched = watched;
+            this.Score = score;
+        }
+    }
+}
